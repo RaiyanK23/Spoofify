5a35c72e6842f4eacc2d27dc0248f8639b4fb203
RaiyanK23 <raiyankbr@gmail.com>
2023-12-10 11:43:09 -0700

    Closing db connection after server terminates

--- server/server.js
+++ server/server.js
@@ -1,30 +1,37 @@
 const express = require('express');
-const db = require('./config/db')
+const db = require('./config/db');
 const cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // Define routes
-app.use('/api/register', require("./routes/register"));
-app.use('/api/auth', require("./routes/auth"));
-app.use('/api/artistAuth', require("./routes/artistAuth"));
+app.use('/api/register', require('./routes/register'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/artistAuth', require('./routes/artistAuth'));
 app.use('/api/playlists', require('./routes/playlist'));
 app.use('/api/logout', require('./routes/logout'));
 
-app.get('/', (req, res) => 
-{
-    return res.json("From Backend");
-})
+app.get('/', (req, res) => {
+  return res.json('From Backend');
+});
 
-app.get('/users', (req, res) =>
-{
-    const sql = "SELECT * FROM users";
-    db.query(sql, (err, data) =>
-    {
-        if(err) return res.json(err);
-        return res.json(data);
-    })
-})
+app.get('/users', (req, res) => {
+  const sql = 'SELECT * FROM users';
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+// Close the MySQL connection when the server terminates
+process.on('exit', () => {
+  db.end();
+  console.log('MySQL connection closed on server exit');
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
 
 app.listen(5000, () => console.log('Server started on port 5000'));
