911cce80764ba88dd6f7378b75203f6e26853b39
Tosho1122 <tosho1122@gmail.com>
2023-12-10 19:33:51 -0700

    Created Search Home and Playlist buttons/Sections

--- client/src/pages/UserPages/UserHome.jsx
+++ client/src/pages/UserPages/UserHome.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom"
 
+import SearchIcon from "../../Images/SearchIcon.png";
+import HomeIcon from "../../Images/HomeIcon.png";
+import PlaylistIcon from "../../Images/PlaylistIcon.png";
+import AddPlaylistIcon from "../../Images/AddPlaylistIcon.png";
+
 import "../../css/UserPagesCSS/UserHome.scss"
 import MusicPlayer from "../../components/MusicPlayer.jsx"
+import PlaylistButton from "../../components/PlaylistButton.jsx"
 
 const  UserHome = () => 
 {
@@ -10,7 +16,58 @@ const  UserHome = () =>
     <div className="userHome">
       
       <div className="window">
-        <p>Hey hey</p>
+
+        {/* Search Home and Playlist area */}
+        <div className="searchHomeAndPlaylist">
+         
+          {/* Search and Home Buttons*/}
+          <div className="searchAndHomeButtons">
+            {/* Search Button*/}
+            <button className="searchButton">
+              <img src={SearchIcon} alt=""/>
+               Search
+            </button>
+
+            {/* Home Button*/}
+            <button className="homeButton">
+              <img src={HomeIcon} alt=""/>
+               Home
+            </button>
+
+          </div>
+          
+          {/* Playlists area */}
+          <div className="playlists">
+            <div className="playlistHeader">
+              <header>
+                <img src={PlaylistIcon} alt=""/>
+                Your Playlists 
+              </header>
+
+              <button className="createPlaylistButton">
+                <img src={AddPlaylistIcon} alt=""/>
+              </button>
+
+            </div>
+
+            <div className="listOfPlaylist">
+              <PlaylistButton/>
+              <PlaylistButton/>
+              <PlaylistButton/>
+              <PlaylistButton/>
+
+
+            </div>
+
+          </div>
+        
+        </div>
+
+        {/* Main Screen area */}
+        <div className="homePage">
+          <p>homePage </p>
+        </div>
+
       </div>
 
       <MusicPlayer/>
