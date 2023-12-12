31a8fba720a2b1b8d38a91d9588727b643740bca
Tosho1122 <tosho1122@gmail.com>
2023-12-12 02:22:00 -0700

    Music player css

--- client/src/components/MusicPlayer.jsx
+++ client/src/components/MusicPlayer.jsx
@@ -3,6 +3,14 @@ import songIcon from "../Images/Graduation.jpeg";
 import audioSrc from "../Audio/Kanye West - Good Morning.mp3"
 import { useState } from 'react';
 
+import{styled, Typography, Slider, Paper, Stack, Box} from '@mui/material';
+
+import nextIcon from "../Images/NextIcon.png";
+import previousIcon from "../Images/PreviousIcon.png";
+import playIcon from "../Images/PlayIcon.png";
+import pauseIcon from "../Images/PauseIcon.png";
+import VolumeIcon from "../Images/VolumeIcon.png";
+
 import "../css/Components/MusicPlayer.scss"
 const  MusicPlayer = () => 
 { 
@@ -13,6 +21,11 @@ const  MusicPlayer = () =>
     setIsPlaying(!isPlaying);
   };
 
+  const togglePlayPause = () => 
+  {
+    setIsPlaying((prev) => !prev);
+  };
+
 
   return(
   <div className="musicPlayerBox">
@@ -21,7 +34,7 @@ const  MusicPlayer = () =>
       {/* Song Image */}
       <img src={songIcon} alt=""/>
 
-      {/* Song Text Infomation */}
+      {/* Song Text Information */}
       <div className="songInfo">
         <span className="mainText"> Good Morning  </span>
         <span className="subText"> Kanye West</span>
@@ -29,13 +42,43 @@ const  MusicPlayer = () =>
 
     </div>
 
-    <div className="musicController">
-     
+    <div className="musicControllerBox">
+      <div  className="middleMusicControls">
+
+        <div className="musicControllers">
+
+          <button className="previousButton">
+              <img src={previousIcon} alt=""/>
+          </button>
+
+          <button className="pausePlay">
+              <img src={pauseIcon} alt=""/>
+          </button>
+
+          <button className="nextButton">
+              <img src={nextIcon} alt=""/>
+          </button>
+        </div>
+          
+        <div  className="bottomMusicControls">
+          <span> 00:00 </span>
+          <Slider className="musicSlider"/>
+          <span> 00:00</span>
+        </div>
+       
+      </div>
+
+      
+
+       {/* Volume control*/}
+      <div className="VolumeControls">
+        <img src={VolumeIcon} alt=""/>
+        <Slider className="volumeSlider"/>
+      </div>
+
     </div>
-
-
-
   </div>
+
   )
 }
 
